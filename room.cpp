2698f4090b98b33ce8f1547bd329ba509fd7e65d
BlayTime <39148329+BlayTime@users.noreply.github.com>
2018-05-16 19:38:32 +0930

    updated room.cpp at 7:39pm

--- room.cpp
+++ room.cpp
@@ -3,22 +3,28 @@
 #include "room.h"
 
 room::room(){
-	room_index = 0;
+	room_X = 0;
+	room_Y = 0;
 	isTrap = 0;
 	playerLocation = 0;
 }
 
-room::room(int index, bool trapYN, bool playerYN)
+room::room(int index_X, int index_Y, bool trapYN, bool playerYN)
 {
-	room_index = index;
+	room_X = index_X;
+	room_Y = index_Y;
 	isTrap = trapYN;
 	playerLocation = playerYN;
 }
 
-int room::get_room_num(){
-	return room_index;
+int room::get_room_numX(){
+	return room_X;
 }
 
-bool room::is_player_in_room(int index){
+int room::get_room_numY(){
+	return room_Y;
+}
+
+bool room::is_player_in_room(int index_X, int index_Y){
 	return playerLocation;
 }
