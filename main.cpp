8543adc23964c52bca6599730a9e016f6a82a59f
BlayTime <39148329+BlayTime@users.noreply.github.com>
2018-05-16 23:54:35 +0930

    Added input function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,40 @@
 #include "room.h"
 using namespace std;
 
+void inputFunc(){
+
+	char strInput; // wont work because of this
+	
+	cout << "Which way? ";
+	cin >> strInput;
+	// getline(cin , strInput)
+
+	//stops taking input if "stop" is entered
+	if(strInput != 'S'){
+		
+		//if input is valid, direction goes through
+		if(strInput == 'w' || strInput == 'a' || strInput == 's' || strInput == 'd'){
+			cout << "You went " << strInput << endl;
+			inputFunc();
+		}
+
+		//tell player the input is invalid --> ask for new input
+		else if(strInput != 'w' || strInput != 'a' || strInput != 's' || strInput != 'd' ){
+			cout << "You entered " << strInput << endl;
+			cout << "There's a time and a place for everything... but not now" << endl;
+			inputFunc();
+		}
+
+
+		//suggested: draw an entity/person on top of the room and a trap on top of them
+		/*void update(entity* e){
+			e.draw()
+		}*/
+	}
+	//if not finished/escaped --> call function again
+
+}
+
 int main(){
 	
 	//Variable to change size of map (mxn matrix)
