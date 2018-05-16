08a02abe0e4b9c4d16157da7a5f10223b0ef9948
mistdiver <39332077+mistdiver@users.noreply.github.com>
2018-05-16 10:31:53 +0000

    added choose_door function to room.cpp

--- room.cpp
+++ room.cpp
@@ -28,3 +28,52 @@ int room::get_room_numY(){
 bool room::is_player_in_room(int index_X, int index_Y){
 	return playerLocation;
 }
+
+void room::choose_door(char direction, room map[3][3]){
+
+	switch (direction){
+		case "W":
+			if (room_Y == 0){
+				std::cout << "don't." << std::endl;
+				return;
+			} 
+			else { 
+				map[room_X][room_Y].playerLocation = 0;
+				map[room_X][room_Y-1].playerLocation = 1;
+				std::cout << "you move up" << std::endl;
+			}
+		case "A":
+			if (room_X == 0 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				map[room_X][room_Y].playerLocation = 0;
+				map[room_X][room_Y-1].playerLocation = 1;
+				std::cout << "you move left" << std::endl;
+			}
+
+		case "S":
+			if (room_Y == 2 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				map[room_X][room_Y].playerLocation = 0;
+				map[room_X][room_Y+1].playerLocation = 1;
+				std::cout << "you move down" << std::endl;
+			}
+
+		case "D":
+			if (room_X == 2 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				map[room_X][room_Y].playerLocation = 0;
+				map[room_x+1][room_Y].playerLocation = 1;
+				std::cout << "you move right" << std::endl;
+			}
+	}
+
+}
