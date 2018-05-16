87c8679ff6a844270560a4efdb701108170aaf83
mistdiver <39332077+mistdiver@users.noreply.github.com>
2018-05-16 11:51:37 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,3 +30,64 @@ int main(){
 	cout << map[1][0].is_player_in_room(1,0) << endl;
 	cout << map[2][0].is_player_in_room(2,0) << endl;
 }
+
+void room::choose_door(char direction, int x, int y){
+
+		/*if (direction == 'W'){
+			if (room_Y == 0){
+				std::cout << "don't." << std::endl;
+				return;
+			} 
+			else { 
+				matrix[room_X][room_Y].playerLocation = 0;
+				matrix[room_X][room_Y-1].playerLocation = 1;
+				std::cout << "you move up" << std::endl;
+			}
+		}
+		if (direction == 'A'){
+			if (room_X == 0 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				matrix[room_X][room_Y].playerLocation = 0;
+				matrix[room_X][room_Y-1].playerLocation = 1;
+				std::cout << "you move left" << std::endl;
+			}
+		}
+		if (direction == 'S'){
+			if (room_Y == 2 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				matrix[room_X][room_Y].playerLocation = 0;
+				matrix[room_X][room_Y+1].playerLocation = 1;
+				std::cout << "you move down" << std::endl;
+			}
+		}*/
+		if (direction == 'D'){
+			if (room_X == 2 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				playerLocation = 0;
+				x +=1;
+				//std::cout << "you move right" << std::endl;
+			}
+		}
+}
+
+void room::movement(char direction){
+		if (direction == 'D'){
+			if (room_X == 2 ){
+				std::cout << "don't." << std::endl;
+				return;
+			}
+			else {
+				playerLocation = 1;
+				std::cout << "you move right" << std::endl;
+			}
+		}
+}
